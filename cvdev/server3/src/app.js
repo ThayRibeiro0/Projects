bcd16b9bc4187a3cc59cbed58c9a80cd301d2697
ThayRibeiro0 <thaysmoiaribeiro@gmail.com>
2022-04-10 22:47:53 -0300

    fit: vai

--- cvdev/server3/src/app.js
+++ cvdev/server3/src/app.js
@@ -2,8 +2,14 @@ import express from "express";
 import db from "../prisma";
 import Users from "./routes/users";
 import { engine } from 'express-handlebars';
+import fs from 'fs';
 
 export const app = express();
+
+const styleslist = fs.readdirSync('./public/styles')
+console.log(styleslist)
+
+app.use(express.static('./public'))
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -12,8 +18,14 @@ app.set('views', './src/views');
 app.use(express.json());
 app.use("/users", Users);
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get('/', (req, res) => {
+  res.redirect('home');
 });
 
+app.get('/home', (req, res) => {
+  res.render('home', { styleslist: styleslist });
+});
 
+app.get('/login', (req, res) => {
+  res.render('login', { styleslist: styleslist });
+});
